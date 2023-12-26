b2ad6827741f7a1f96d0f47199dbb496776a91bf
Artem Borovik <polzovatellllk@gmail.com>
2023-12-26 17:05:24 +0300

    windows fix

--- platforms/Windows/build.ps1
+++ platforms/Windows/build.ps1
@@ -4,6 +4,3 @@ $BuildConfig = $args[0]
 echo "Build config: $BuildConfig"
 cmake -S platforms/Windows/ -B platforms/Windows/build -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL
 cmake --build platforms/Windows/build -t pack_artifacts --config $BuildConfig --parallel
-mkdir .\artifacts\mods
-cp .\mods\core\core.wrmod .\artifacts\mods\
-cp .\mods\basecontent\basecontent.wrmod .\artifacts\mods\
