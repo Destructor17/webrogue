70f1398e36faf384106ae8b623197da95a522229
Artem Borovik <polzovatellllk@gmail.com>
2024-01-03 18:40:57 +0300

    reorganize a bit

--- scripts/webtest.sh
+++ scripts/webtest.sh
@@ -6,13 +6,11 @@ emcmake cmake -Bplatforms/Web/game/build -Splatforms/Web/game/ -DCMAKE_BUILD_TYP
 cmake --build platforms/Web/game/build/ --target pack_artifacts -j || exit 2
 
 mv artifacts game_artifacts || exit 2
-mv game_artifacts/index.html game_artifacts/game.html || exit 2
 
 emcmake cmake -Bplatforms/Web/homepage/build -Splatforms/Web/homepage/ -DCMAKE_BUILD_TYPE=Release || exit 2
 cmake --build platforms/Web/homepage/build/ --target pack_artifacts -j || exit 2
 
-mv game_artifacts/* artifacts || exit 2
-rmdir game_artifacts || exit 2
+mv game_artifacts artifacts/game || exit 2
 
 
 python3 scripts/srv.py
